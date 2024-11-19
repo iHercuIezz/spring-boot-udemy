9a79c2e08700cfb69b43d4f48d4d2b8be2531f29
Rijde Imano <imano.rijde@student.howest.be>
2024-11-19 19:55:19 +0100

    Init

---
+++ src/test/java/com/springboot/udemycourse/UdemycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.udemycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
