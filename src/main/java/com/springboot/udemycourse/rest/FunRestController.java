dcd3d28850a8ef077544cf25b750e580d627d5d6
Rijde Imano <imano.rijde@student.howest.be>
2024-11-26 11:39:28 +0100

    properties

--- src/main/java/com/springboot/udemycourse/rest/FunRestController.java
+++ src/main/java/com/springboot/udemycourse/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.springboot.udemycourse.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return String.format("Coach: %1$s, Team: %2$s", coachName, teamName);
+    }
 
     @GetMapping("/")
     public String sayHello() {
