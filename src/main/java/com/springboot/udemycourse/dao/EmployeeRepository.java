34bdba694d5c3c13d1a8c46d2fae0c6e4023e73a
Imano Rijde <ixrijde@gmail.com>
2024-12-02 14:56:30 +0100

    rest-crud-employee-zith-spring-data-rest

--- src/main/java/com/springboot/udemycourse/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/udemycourse/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.springboot.udemycourse.dao;
 
 import com.springboot.udemycourse.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
